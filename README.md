fa848479164c8af4777f5776da31da8b0bac87b9
Nick Matantsev <nick.matantsev@gmail.com>
2016-03-05 14:58:54 -0500

    Added Slack token check, instructions

--- README.md
+++ README.md
@@ -7,8 +7,15 @@ npm install
 
 cat <<EOF > env.sh
 export TARGET_GIT_URL=https://<user>:<personal-token>@github.com/<user>/<repo>.git
+export SLACK_TOKEN=<token>
 EOF
 
+# repo tests
 . env.sh
-node index.js
+supervisor index.js
+
+# Slack tests
+. env.sh
+ngrok http 3010 # then get the URL and add to Slack as outgoing hook in a dedicated channel
+supervisor slack.js
 ```
--- slack.js
+++ slack.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var slackHookToken = process.env.SLACK_TOKEN || '';
 var configuredPort = process.env.PORT || 3010;
 
 var app = express();
@@ -8,6 +9,12 @@ var app = express();
 app.get('/', function (req, res) { res.send(''); }); // for looky-loos
 
 app.post('/', bodyParser.urlencoded({ extended: true }), function (req, res) {
+    if (req.body.token !== slackHookToken) {
+        res.status(403);
+        res.send(''); // nothing to do
+        return;
+    }
+
     if (req.body.bot_id) {
         res.send(''); // nothing to do
         return;
