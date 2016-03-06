99558f7d7f790c0ecaeaa0e000c23073cb54850a
Nick Matantsev <nick.matantsev@gmail.com>
2016-03-06 00:41:03 -0500

    Responding on successful file processing event

--- slack.js
+++ slack.js
@@ -1,7 +1,35 @@
+var Promise = require('bluebird');
 var Slack = require('slack-client');
 var request = require('request');
 
 var slackAuthToken = process.env.SLACK_AUTH_TOKEN || '';
+
+function getSlackFile(file) {
+    return new Promise(function (resolve, reject) {
+        var fileUrl = file.url_private_download;
+
+        console.log('file request', fileUrl);
+
+        request.get({
+            url: fileUrl,
+            auth: { bearer: slackAuthToken },
+            encoding: null // ensure binary response
+        }, function (err, resp, body) {
+            console.log('file response', fileUrl);
+
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(body);
+        });
+    });
+}
+
+function escapeSlackText(text) {
+    return text.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
+}
 
 var slackClient = new Slack.RtmClient(slackAuthToken);
 
@@ -11,21 +39,31 @@ slackClient.on(Slack.CLIENT_EVENTS.RTM.AUTHENTICATED, function () {
 
 slackClient.start();
 
-slackClient.on(Slack.RTM_EVENTS.FILE_SHARED, function (e) {
+slackClient.on(Slack.RTM_EVENTS.MESSAGE, function (e) {
+    // ignore anything but file shares
+    if (e.subtype !== 'file_share') {
+        return;
+    }
+
+    var channelId = e.channel;
     var file = e.file;
 
-    console.log('created file', file.id, file.name);
+    // ignore anything but channel messages
+    if (!channelId) {
+        return;
+    }
 
-    var fileUrl = file.url_private_download;
+    console.log('shared file', file.id, file.name, channelId);
 
-    console.log('file request', fileUrl);
+    getSlackFile(file).then(function (body) {
+        console.log('got data', body.length);
+    }).then(function () {
+        console.log('successfully processed slack upload', file.name);
 
-    request.get({
-        url: fileUrl,
-        auth: { bearer: slackAuthToken },
-        encoding: null // ensure binary response
-    }, function (err, resp, body) {
-        console.log('file response', err, Buffer.isBuffer(body), body.length);
+        slackClient.sendMessage('processed file: ' + escapeSlackText(file.name), channelId);
+    }, function (err) {
+        console.error('error processing slack upload', file.name, err);
+
+        slackClient.sendMessage('error processing file: ' + escapeSlackText(file.name), channelId);
     });
 });
-
