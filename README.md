40d5d53cbf5e6bde93e266d198a86b4fd611f672
Nick Matantsev <nick.matantsev@gmail.com>
2016-03-05 22:07:16 -0500

    Using Slack RTM API

--- README.md
+++ README.md
@@ -15,7 +15,6 @@ cat <<EOF > env.sh
 export TARGET_GIT_URL=https://<user>:<personal-token>@github.com/<user>/<repo>.git
 
 export SLACK_AUTH_TOKEN=<auth-token>
-export SLACK_HOOK_TOKEN=<hook-token>
 EOF
 
 # repo tests
@@ -24,6 +23,5 @@ supervisor index.js
 
 # Slack tests
 . env.sh
-ngrok http 3010 # then get the URL and add to Slack as outgoing hook in a dedicated channel
 supervisor slack.js
 ```
--- slack.js
+++ slack.js
@@ -1,75 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+var Slack = require('slack-client');
 
 var slackAuthToken = process.env.SLACK_AUTH_TOKEN || '';
-var slackHookToken = process.env.SLACK_HOOK_TOKEN || '';
-var configuredPort = process.env.PORT || 3010;
 
-var FILE_TIMESTAMP_MARGIN = 3; // how many seconds into the past to look before message timestamp for its files
+var slackClient = new Slack.RtmClient(slackAuthToken);
 
-var app = express();
-
-app.get('/', function (req, res) { res.send(''); }); // for looky-loos
-
-app.post('/', bodyParser.urlencoded({ extended: true }), function (req, res) {
-    if (req.body.token !== slackHookToken) {
-        res.status(403);
-        res.send(''); // nothing to do
-        return;
-    }
-
-    if (req.body.bot_id) {
-        res.send(''); // nothing to do
-        return;
-    }
-
-    var channelId = req.body.channel_id;
-    var timestampFloor = Math.floor(req.body.timestamp);
-
-    res.send({ text: 'thanks, received!' });
-
-    function trySearch(count) {
-        if (count > 20) {
-            console.log('giving up');
-            return;
-        }
-
-        setTimeout(function () {
-            console.log(
-                'searching', {
-                    "token": slackAuthToken,
-                    "channel": channelId,
-                    ts_from: timestampFloor - FILE_TIMESTAMP_MARGIN,
-                    ts_to: timestampFloor + FILE_TIMESTAMP_MARGIN
-                }
-            );
-
-            request.post({
-                url: 'https://slack.com/api/files.list',
-                json: true,
-                qs: {
-                    "token": slackAuthToken,
-                    "channel": channelId,
-                    ts_from: timestampFloor - FILE_TIMESTAMP_MARGIN,
-                    ts_to: timestampFloor
-                },
-            }, function(err, resp, body) {
-                if (!body || !body.ok) {
-                    console.error('slack err:', err, body);
-                    return;
-                }
-
-                console.log('slack files:', body.files);
-
-                if (body.files.length < 1) {
-                    trySearch(count + 1);
-                }
-            });
-        }, 5000);
-    }
-
-    trySearch(0);
+slackClient.on(Slack.CLIENT_EVENTS.RTM.AUTHENTICATED, function () {
+    console.log('started slack client');
 });
 
-app.listen(configuredPort);
+slackClient.start();
+
+slackClient.on(Slack.RTM_EVENTS.FILE_SHARED, function (e) {
+    console.log('created file', e);
+});
+
